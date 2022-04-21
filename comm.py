005ab051f49e9cbc9f5a5136bfa0056a2cabd035
Bottram <thom.bottram@alliancellc.com>
2022-04-21 16:09:23 +0200

    Example configuration
    Example configuration, already tested and works

--- comm.py
+++ comm.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # comm.py>
 from configparser import *
-import sys, snap7
+import sys, logging, snap7, time
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%H:%M:%S', level=logging.DEBUG)
 
 # Define configuration
 def config():
@@ -12,27 +14,46 @@ def config():
         var2 = config.get("DEFAULT", "port")
         return var1, var2
     except Exception as e:
-        print("[+] ERROR,", e)
+        logging.error(e)
         return -1
 
 # Define main function
 def main(var1, var2):
     # Read configuration
     ip = config()[0]
-    port = config()[1]
+    port = int(config()[1])
     if ip == -1 or port == -1:
         return -1
 
+    db10_layout="""
+    0           id              INT
+    2           name            STRING[6]
+    10.0        testbool1       BOOL
+    10.1        testbool2       BOOL
+    10.2        testbool3       BOOL
+    10.3        testbool4       BOOL
+    10.4        testbool5       BOOL
+    10.5        testbool6       BOOL
+    10.6        testbool7       BOOL
+    10.7        testbool8       BOOL
+    12          testreal        REAL
+    16          testdword       DWORD"""
+
     try:
-        print("[+] INFO, connecting")
         client = snap7.client.Client()
         client.connect(ip, 0, 0, port)
         client.get_connected()
-        data = client.db_read(var1, var2, 4)
-        print(data)
+        all_data = client.db_read(var1, 0, 20)
+        db10 = snap7.util.DB(0, all_data, db10_layout, 0, 1)
+
+        print(db10[0])
+        #print(db10[0]["testbool3"])
+        #print(db10[0])
     except Exception as e:
-        print("[+] ERROR,", e)
+        #logging.error(e)
         return -1
 
-if main("1", "0") == -1:
-    sys.exit()
+while 1:
+    if main(1, 0) == -1:
+        sys.exit()
+    time.sleep(3)
