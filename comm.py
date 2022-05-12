c1a1e84cd7d7695b8cb4de20d371eb4b20a288ce
Bottram <thom.bottram@alliancellc.com>
2022-05-12 15:08:32 +0200

    Updated Main code
    Send and receive tested and fully completed

--- comm.py
+++ comm.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 # comm.py>
 from configparser import *
-import sys, logging, snap7, time
+import logging, snap7
 
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%H:%M:%S')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
@@ -13,43 +12,60 @@ def config():
         config = ConfigParser()
         config.read("config.ini")
         var1 = config.get("DEFAULT", "ip")
-        var2 = config.get("DEFAULT", "port")
-        var3 = config.get("DEFAULT", "db1")
-        var4 = config.get("DEFAULT", "db2")
-        return var1, var2, var3, var4
+        var2 = int(config.get("DEFAULT", "port"))
+        return var1, var2
     except Exception as e:
         logger.error(e)
         return -1
 
 # Define main function
-def main(var1, var2):
+def main(var, **data):
     # Read configuration
     ip = config()[0]
-    port = int(config()[1])
-    db1_layout = config()[2]
-    db2_layout = config()[3]
-    if ip == -1 or port == -1 or db1_layout == -1 or db2_layout == -1:
+    port = config()[1]
+    if ip == -1 or port == -1:
         return -1
 
     # Connect to client
     try:
         client = snap7.client.Client()
         client.connect(ip, 0, 0, port)
-    except:
+    except Exception as e:
+        logger.error(e)
         return -1
     
     # While connected to client
     while client.get_connected() == True:
         try:
-            all_data = client.db_read(var1, 0, var2)
-            db1 = snap7.util.DB(0, all_data, db1_layout, 0, 1)
-
-            all_data = client.db_read(var1 + 1, 0, var2 - 10)
-            db2 = snap7.util.DB(0, all_data, db2_layout, 0, 1)
-
-            #logger.info(db10[0])
-            #print(db10[0]["testbool3"])
-            #print(db10[0])
-            return db1, db2
-        except:
+            all_data = client.db_read(data.get('db'), 0, data.get('size'))
+            db_data = snap7.util.DB(0, all_data, data.get('layout'), 0, 1)
+            if var == "send":
+                db_data[0][data.get('id')] = data.get('val')
+                client.db_write(data.get('db'), 0, db_data._bytearray)
+                return
+            if var == "recv":
+                return db_data
+        except Exception as e:
+            logger.error(e)
             return -1
+
+# DEBUG TEST
+if __name__ == "__main__":
+    test = "send"
+    list= { "db":2,
+            "size":10,
+            "id":"id1",
+            "val":99,
+            "layout":"""
+0           id1             INT
+2           id2             INT
+4           id3             INT
+6           id4             INT
+8           id5             INT
+"""
+        }
+    data = main(test, **list)
+    if data != -1 and test == "recv":
+        print(data[0])
+    else:
+        print(data)
--- test-ui.py
+++ test-ui.py
@@ -3,7 +3,7 @@ from tkinter import *
 from tkinter import font
 from tkinter import messagebox
 from configparser import *
-import sys, comm
+import sys, logging, comm
 
 root=Tk() # Create main window
 root.title('Test UI') # Window title
@@ -11,16 +11,60 @@ root.configure(bg='white') # Window background
 root.minsize(400, 200) # Window size minimum
 #root.state('zoomed') # Normal fullscreen
 #root.attributes("-fullscreen", True) # Canvas fullscreen
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%H:%M:%S')
 
-def updater():  
-    text1['text'] = comm.main(1, 20)[1][0]["id1"]
+# Define configuration
+def config():
+    var1 = [[],[],[]]
+    var2 = [[],[],[]]
+    try:
+        config = ConfigParser()
+        config.read("config.ini")
+        var1[0] = int(config.get("DB1", "address"))
+        var1[1] = int(config.get("DB1", "size"))
+        var1[2] = config.get("DB1", "layout")
+        var2[0] = int(config.get("DB2", "address"))
+        var2[1] = int(config.get("DB2", "size"))
+        var2[2] = config.get("DB2", "layout")
+        return var1, var2
+    except:
+        return -1
+
+def updater():
+    data= { "db":db2[0],
+            "size":db2[1],
+            "layout":db2[2]
+        }
+    data = comm.main("recv", **data)[0]
+    if data != -1:
+        text1['text'] = data["id1"]
     root.after(1000, updater)
+
+def submit():
+    data= { "db":db2[0],
+            "size":db2[1],
+            "layout":db2[2],
+            "id":"id1",
+            "val":int(entry1.get())
+        }
+    comm.main("send", **data)
+
 
 # Define sub window
 text1 = Label(root, text="Not connected")
+entry1 = Entry(root, text="")
+btn1 = Button(root, text="send", command=submit)
 
 # Place root objects in order
 text1.pack(side=TOP)
+entry1.pack(side=TOP)
+btn1.pack(side=BOTTOM)
 
-updater()
-mainloop() # Running mainloop always last
+# Read configuration
+db1 = config()[0]
+db2 = config()[1]
+if db1 == -1 or db2 == -1:
+    sys.exit()
+else:
+    updater()
+    mainloop() # Running mainloop always last
